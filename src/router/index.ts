de0d09b8d4080bf4fc36605f9cf28957c092dda7
Fajar Bagus Widiarno <widiarnofajarbagus@gmail.com>
2026-08-11 08:43:41 +0700

    new

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
-import NotFoundView from "../views/NotFoundView.vue";
-import BeritaView from "@/views/pages/BeritaView.vue";
-
 const routes: RouteRecordRaw[] = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/berita/:id",
-    name: "berita",
-    component: BeritaView,
-  },
-  {
-    path: "/:pathMatch(.*)*",
-    name: "not-found",
-    component: NotFoundView,
-  },
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
+  // {
+  //   path: "/berita/:id",
+  //   name: "berita",
+  //   component: BeritaView,
+  // },
+  // {
+  //   path: "/:pathMatch(.*)*",
+  //   name: "not-found",
+  //   component: NotFoundView,
+  // },
 ];
 
 const router = createRouter({
